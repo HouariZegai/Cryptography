5eef1e250ccbecd4809dcfa21174767250d3cf54
HouariZEGAI <houarizegai14@gmail.com>
2018-11-25 22:43:49 +0100

    Adding Crypto Algo problem #1

---
+++ src/com/houarizegai/crypto/crypto_algo1/Crypto1.java
@@ -0,0 +1,43 @@
+package com.houarizegai.crypto.crypto_algo1;
+
+import java.util.Scanner;
+
+public class Crypto1 {
+
+    private final static String[][] POLYBE = {
+        {"A", "B", "C", "D", "E"},
+        {"F", "G", "H", "IJ", "K"},
+        {"L", "M", "N", "O", "P"},
+        {"Q", "R", "S", "T", "U"},
+        {"V", "W", "X", "Y", "Z"}
+    };
+    
+    public static void main(String[] args) {
+        
+        Scanner mScanner = new Scanner(System.in);
+        System.out.print("Type Plain Message: ");
+        String msgPlain = mScanner.nextLine();
+        
+        String msgCrypte = getMsgCrypte(msgPlain);
+        System.out.println("Msg ctypte : " + msgCrypte);
+    }
+    
+    private static String getMsgCrypte(String msgPlain) {
+        String msgCrypte = "";
+        
+        for(char c : msgPlain.toCharArray()) {
+            loopTable:
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 5; j++) {
+                    if(POLYBE[i][j].contains(String.valueOf(c).toUpperCase())) {
+                        msgCrypte += i;
+                        msgCrypte += j;
+                        break loopTable;
+                    }
+                }
+            }
+        }
+        
+        return msgCrypte;
+    }    
+}
