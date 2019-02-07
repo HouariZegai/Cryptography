f5b7d57447387fad7b3f6348e79cb8eb3e0011f5
HouariZEGAI <houarizegai14@gmail.com>
2019-02-07 23:22:30 +0100

    Cleaning all code

--- src/com/houarizegai/main/Main.java
+++ src/com/houarizegai/main/Main.java
@@ -3,12 +3,13 @@ package com.houarizegai.main;
 import com.houarizegai.crypto.Feistel;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println(new Feistel().encrypt("01001001", "1010", 4));
+        String plainText = "01001001";
+        String key = "1010";
+        int round = 4;
+
+        Feistel feistel = new Feistel();
+        System.out.println(feistel.encrypt(plainText, key, round));
         
     }
-    
-    
-    
 }
